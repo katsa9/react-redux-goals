bd934bc41d9c70a252aedd77fe1c0b730e1fe260
katsa9 <kathleen.brand2@gmail.com>
2019-06-25 11:46:55 +0200

    add reducer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function createStore () {
+//Library Code
+function createStore (reducer) {
   //the store should have 4 parts
   // 1. the state
   // 2. get the state
@@ -9,11 +10,17 @@ function createStore () {
   let listeners = []
 
   const getState = () => state
+
   const subscribe = (listener) => {
     listeners.push(listener);
     return () => { //when subscribing, we return a function that can then be used to unsubscribe
       listeners = listeners.filter((l) => l !== listener)
     }
+  }
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
   }
 //return object containing the getState function
   return {
@@ -22,10 +29,19 @@ function createStore () {
   }
 }
 
-const store = createStore();
-store.subscribe(() => {
-  console.log("the new state is", store.getState();
-});
-const unsubscribe = store.subscribe(() => {
-  console.log("the state changed");
-});
+//App Code
+function todos (state = [], action) { //this is called a reducer - reduces the current state and the action to the new state
+  if(action.type === 'ADD_TODO'){
+    return state.concat([action.todo]);
+  }
+  return state;
+}
+
+
+// const store = createStore();
+// store.subscribe(() => {
+//   console.log("the new state is", store.getState();
+// });
+// const unsubscribe = store.subscribe(() => {
+//   console.log("the state changed");
+// });
